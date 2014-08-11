8770fcd032844bbc61f877f8ecf634d09897e2d1
karthy07 <mail2karthyk@gmail.com>
2014-08-11 15:19:54 +0530

    BuildMatrix
    3D Matrix Built

--- Assets/Scripts/ScriptHandler.cs
+++ Assets/Scripts/ScriptHandler.cs
@@ -13,8 +13,137 @@ public class ScriptHandler : MonoBehaviour {
         currRubix = new GameObject[3, 3, 3];
     }
 
+    void Start()
+    {
+        BuildMatrix();
+    }
+
     void BuildMatrix()
     {
+        GameObject[] cubes = GameObject.FindGameObjectsWithTag("Cube");
 
+        foreach (GameObject cube in cubes)
+        {
+            //Front Facing Cubes
+            if (cube.transform.position == new Vector3(-cubeSize, cubeSize, -cubeSize))
+                currRubix[0, 0, 0] = cube;
+            if (cube.transform.position == new Vector3(0, cubeSize, -cubeSize))
+                currRubix[0, 1, 0] = cube;
+            if (cube.transform.position == new Vector3(cubeSize, cubeSize, -cubeSize))
+                currRubix[0, 2, 0] = cube;
+            if (cube.transform.position == new Vector3(-cubeSize, 0, -cubeSize))
+                currRubix[1, 0, 0] = cube;
+            if (cube.transform.position == new Vector3(0, 0, -cubeSize))
+                currRubix[1, 1, 0] = cube;
+            if (cube.transform.position == new Vector3(cubeSize, 0, -cubeSize))
+                currRubix[1, 2, 0] = cube;
+            if (cube.transform.position == new Vector3(-cubeSize, -cubeSize, -cubeSize))
+                currRubix[2, 0, 0] = cube;
+            if (cube.transform.position == new Vector3(0, -cubeSize, -cubeSize))
+                currRubix[2, 1, 0] = cube;
+            if (cube.transform.position == new Vector3(-cubeSize, -cubeSize, -cubeSize))
+                currRubix[2, 2, 0] = cube;
+
+            //Back Facing Cubes
+            if (cube.transform.position == new Vector3(-cubeSize, cubeSize, cubeSize))
+                currRubix[0, 0, 2] = cube;
+            if (cube.transform.position == new Vector3(0, cubeSize, cubeSize))
+                currRubix[0, 1, 2] = cube;
+            if (cube.transform.position == new Vector3(cubeSize, cubeSize, cubeSize))
+                currRubix[0, 2, 2] = cube;
+            if (cube.transform.position == new Vector3(-cubeSize, 0, cubeSize))
+                currRubix[1, 0, 2] = cube;
+            if (cube.transform.position == new Vector3(0, 0, cubeSize))
+                currRubix[1, 1, 2] = cube;
+            if (cube.transform.position == new Vector3(cubeSize, 0, cubeSize))
+                currRubix[1, 2, 2] = cube;
+            if (cube.transform.position == new Vector3(-cubeSize, -cubeSize, cubeSize))
+                currRubix[2, 0, 2] = cube;
+            if (cube.transform.position == new Vector3(0, -cubeSize, cubeSize))
+                currRubix[2, 1, 2] = cube;
+            if (cube.transform.position == new Vector3(cubeSize, -cubeSize, cubeSize))
+                currRubix[2, 2, 2] = cube;
+
+            //Left Facing Cubes
+            if (cube.transform.position == new Vector3(-cubeSize, cubeSize, -cubeSize))
+                currRubix[0, 0, 0] = cube;
+            if (cube.transform.position == new Vector3(-cubeSize, cubeSize, 0))
+                currRubix[0, 0, 1] = cube;
+            if (cube.transform.position == new Vector3(-cubeSize, cubeSize, cubeSize))
+                currRubix[0, 0, 2] = cube;
+            if (cube.transform.position == new Vector3(-cubeSize, 0, -cubeSize))
+                currRubix[1, 0, 0] = cube;
+            if (cube.transform.position == new Vector3(-cubeSize, 0, 0))
+                currRubix[1, 0, 1] = cube;
+            if (cube.transform.position == new Vector3(-cubeSize, 0, cubeSize))
+                currRubix[1, 0, 2] = cube;
+            if (cube.transform.position == new Vector3(-cubeSize, -cubeSize, -cubeSize))
+                currRubix[2, 0, 0] = cube;
+            if (cube.transform.position == new Vector3(-cubeSize, -cubeSize, 0))
+                currRubix[2, 0, 1] = cube;
+            if (cube.transform.position == new Vector3(-cubeSize, -cubeSize, cubeSize))
+                currRubix[2, 0, 2] = cube;
+
+            //Right Facing Cubes
+            if (cube.transform.position == new Vector3(cubeSize, cubeSize, -cubeSize))
+                currRubix[0, 2, 0] = cube;
+            if (cube.transform.position == new Vector3(cubeSize, cubeSize, 0))
+                currRubix[0, 2, 1] = cube;
+            if (cube.transform.position == new Vector3(cubeSize, cubeSize, cubeSize))
+                currRubix[0, 2, 2] = cube;
+            if (cube.transform.position == new Vector3(cubeSize, 0, -cubeSize))
+                currRubix[1, 2, 0] = cube;
+            if (cube.transform.position == new Vector3(cubeSize, 0, 0))
+                currRubix[1, 2, 1] = cube;
+            if (cube.transform.position == new Vector3(cubeSize, 0, cubeSize))
+                currRubix[1, 2, 2] = cube;
+            if (cube.transform.position == new Vector3(cubeSize, -cubeSize, -cubeSize))
+                currRubix[2, 2, 0] = cube;
+            if (cube.transform.position == new Vector3(cubeSize, -cubeSize, 0))
+                currRubix[2, 2, 1] = cube;
+            if (cube.transform.position == new Vector3(cubeSize, -cubeSize, cubeSize))
+                currRubix[2, 2, 2] = cube;
+
+            //Up Facing Cubes
+            if (cube.transform.position == new Vector3(-cubeSize, cubeSize, -cubeSize))
+                currRubix[0, 0, 0] = cube;
+            if (cube.transform.position == new Vector3(0, cubeSize, -cubeSize))
+                currRubix[0, 1, 0] = cube;
+            if (cube.transform.position == new Vector3(cubeSize, cubeSize, -cubeSize))
+                currRubix[0, 2, 0] = cube;
+            if (cube.transform.position == new Vector3(-cubeSize, cubeSize, 0))
+                currRubix[0, 0, 1] = cube;
+            if (cube.transform.position == new Vector3(0, cubeSize, 0))
+                currRubix[0, 1, 1] = cube;
+            if (cube.transform.position == new Vector3(cubeSize, cubeSize, 0))
+                currRubix[0, 2, 1] = cube;
+            if (cube.transform.position == new Vector3(-cubeSize, cubeSize, cubeSize))
+                currRubix[0, 0, 2] = cube;
+            if (cube.transform.position == new Vector3(0, cubeSize, cubeSize))
+                currRubix[0, 1, 2] = cube;
+            if (cube.transform.position == new Vector3(cubeSize, cubeSize, cubeSize))
+                currRubix[0, 2, 2] = cube;
+
+            //Down Facing Cubes
+            if (cube.transform.position == new Vector3(-cubeSize, -cubeSize, -cubeSize))
+                currRubix[2, 0, 0] = cube;
+            if (cube.transform.position == new Vector3(0, -cubeSize, -cubeSize))
+                currRubix[2, 1, 0] = cube;
+            if (cube.transform.position == new Vector3(cubeSize, -cubeSize, -cubeSize))
+                currRubix[2, 2, 0] = cube;
+            if (cube.transform.position == new Vector3(-cubeSize, -cubeSize, 0))
+                currRubix[2, 0, 1] = cube;
+            if (cube.transform.position == new Vector3(0, -cubeSize, 0))
+                currRubix[2, 1, 1] = cube;
+            if (cube.transform.position == new Vector3(cubeSize, -cubeSize, 0))
+                currRubix[2, 2, 1] = cube;
+            if (cube.transform.position == new Vector3(-cubeSize, -cubeSize, cubeSize))
+                currRubix[2, 0, 2] = cube;
+            if (cube.transform.position == new Vector3(0, -cubeSize, cubeSize))
+                currRubix[2, 1, 2] = cube;
+            if (cube.transform.position == new Vector3(cubeSize, -cubeSize, cubeSize))
+                currRubix[2, 2, 2] = cube;
+        }
+             
     }
 }
