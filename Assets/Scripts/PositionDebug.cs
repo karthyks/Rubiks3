8edd7c5c2907dcd2a7c2cc094e642dc77fb66695
karthy07 <mail2karthyk@gmail.com>
2014-08-11 14:36:28 +0530

    CommitFirst
    Only Models no Codes

---
+++ Assets/Scripts/PositionDebug.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PositionDebug : MonoBehaviour {
+
+    void Start()
+    {
+        Debug.Log(this.transform.position);
+    }
+}
---
+++ Assets/Scripts/ScriptHandler.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScriptHandler : MonoBehaviour {
+
+    GameObject[, ,] startRubix;
+    GameObject[, ,] currRubix;
+
+    public float cubeSize = 0.3048f;
+    void Awake()
+    {
+        startRubix = new GameObject[3, 3, 3];
+        currRubix = new GameObject[3, 3, 3];
+    }
+
+    void BuildMatrix()
+    {
+
+    }
+}
